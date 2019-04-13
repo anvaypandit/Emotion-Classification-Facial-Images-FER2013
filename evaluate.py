dcd4341ef71c66760a4722c8fa8fe7455a9a2d86
agpandit <43184200+agpandit@users.noreply.github.com>
2019-04-13 15:22:45 -0500

    Evaluate Script Added

---
+++ evaluate.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+def evaluate_class_accuracy(result_dir='Results',result_file_name='Results.csv'):
+    fp = os.path.join(os.getcwd(),result_dir)
+    fp = os.path.join(fp,result_file_name)
+    df = pd.read_csv(fp)
+    correct_pred = 0
+    total_pred = 0
+
+    for row in df.itertuples():
+        total_pred += 1
+        imageLabel = row.Filename.split('/')[1]
+        trueLabel = int(imageLabel[0])
+        if trueLabel == int(row.Predictions):
+            correct_pred += 1
+    accuracy = (correct_pred / total_pred)
+    return accuracy
+
+
+def test(result_file_name='Results.csv'):
+    return evaluate_class_accuracy(result_file_name=result_file_name)
+
+
+print(test('Results.csv'))
